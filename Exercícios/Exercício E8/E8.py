7a6b3c2d4ff67357a9423b688de1fc104de260ed
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-05-24 22:42:07 -0300

    Update E8.py

--- Exercícios/Exercício E8/E8.py
+++ Exercícios/Exercício E8/E8.py
@@ -16,6 +16,21 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    type = db.Column(db.String(50))
+    __mapper_args__ = {
+        'polymorphic_identity': 'animal',
+        'polymorphic_on': type
+    }
+
+
+class Dog(Animal):
+    __mapper_args__ = {
+        'polymorphic_identity': 'dog'
+    }
+    breed = db.Column(db.String(50))
 
 
 db.create_all()
