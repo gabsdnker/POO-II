cf075215f9ed54d3034ba02109c68319b922ec43
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-05-24 22:45:19 -0300

    Update E8.py

--- Exercícios/Exercício E8/E8.py
+++ Exercícios/Exercício E8/E8.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -16,7 +17,9 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-    class Animal(db.Model):
+
+
+class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     type = db.Column(db.String(50))
@@ -33,5 +36,38 @@ class Dog(Animal):
     breed = db.Column(db.String(50))
 
 
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    date = db.Column(db.Date, default=date.today)
+
+
+# Configurar a URI do banco de dados MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/db_name'
+
+# Criar todas as tabelas no banco de dados
 db.create_all()
+
+# Incluir um autor
+author1 = Author(name='Author 1')
+db.session.add(author1)
+db.session.commit()
+
+# Consultar todos os autores
+authors = Author.query.all()
+for author in authors:
+    print(author.name)
+
+# Consultar autores por filtro
+authors_filtered = Author.query.filter_by(name='Author 1').all()
+for author in authors_filtered:
+    print(author.name)
+
+# Alterar um autor
+author1.name = 'New Author Name'
+db.session.commit()
+
+# Excluir um autor
+db.session.delete(author1)
+db.session.commit()
 
