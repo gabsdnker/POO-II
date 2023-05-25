23d2977e81838376ff5a7af249869a5fac3944e7
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-05-24 22:41:25 -0300

    Update E8.py

--- Exercícios/Exercício E8/E8.py
+++ Exercícios/Exercício E8/E8.py
@@ -1,1 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    books = db.relationship('Book', backref='author', cascade='all, delete')
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+
+
+db.create_all()
 
